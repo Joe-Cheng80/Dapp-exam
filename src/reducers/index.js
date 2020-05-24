bede69693699aa61bbed22385cc71c833c4424e4
Joe-Cheng80 <joecheng91@hotmail.com>
2020-05-24 14:56:52 +0800

    新增reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,17 @@
-const initialState = {};
+import { FETCH_ASSETS_LIST_FULFILLED } from "../actions";
+
+const initialState = {
+	list: [],
+	detail: {}
+};
 
 const assets = (state = initialState, action) => {
 	switch (action.type) {
+		case FETCH_ASSETS_LIST_FULFILLED:
+			return {
+				...state,
+				list: [...state.list, ...action.response.assets]
+			};
 		default:
 			return state;
 	}
