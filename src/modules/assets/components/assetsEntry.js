e7de346549303c3209ff8c9e0dcc79bc75466933
Joe-Cheng80 <joecheng91@hotmail.com>
2020-05-24 22:44:41 +0800

    優化assetsEntry render邏輯

--- src/modules/assets/components/assetsEntry.js
+++ src/modules/assets/components/assetsEntry.js
@@ -4,20 +4,21 @@ import { Route, Switch, Redirect } from "react-router-dom";
 const Assets = lazy(() => import("../containers/assets"));
 const Detail = lazy(() => import("../containers/detail"));
 
-const AssetsEntry = props => (
-	<>
-		<Suspense fallback={<div>loading</div>}>
-			<Switch>
-				<Route
-					path="/assets/detail/:contractAddress/:tokenId"
-					component={Detail}
-				/>
-				<Route path="/assets/:address" component={Assets} />
-				<Redirect from="/" to={`/assets/${props.account}`} />
-				<Route render={() => <div>NOT FOUND</div>} />
-			</Switch>
-		</Suspense>
-	</>
-);
+const AssetsEntry = props =>
+	props.account.length > 0 && (
+		<>
+			<Suspense fallback={<div>loading</div>}>
+				<Switch>
+					<Route
+						path="/assets/detail/:contractAddress/:tokenId"
+						component={Detail}
+					/>
+					<Route path="/assets/:address" component={Assets} />
+					<Redirect from="/" to={`/assets/${props.account}`} />
+					<Route render={() => <div>NOT FOUND</div>} />
+				</Switch>
+			</Suspense>
+		</>
+	);
 
 export default AssetsEntry;
